fbaae75abc6c7599e8dda34fbe46b6eb1bc0ff1b
Adith628 <adithsreedhar628@gmail.com>
2025-11-09 15:52:20 +0530

    [feat] : ticket c ontrollers completed

--- ai-ticket-agent/index.js
+++ ai-ticket-agent/index.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-created.js";
 
 dotenv.config();
 
@@ -12,7 +17,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
-
+app.use("/api/ticket", ticketRoutes);
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
